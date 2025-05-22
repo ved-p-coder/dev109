4f170cd7b189fe70b781c43c13bbc811e39951bd
ved-p-coder <ved.patel@bellevuecollege.edu>
2025-05-22 16:27:09 -0700

    Update rhombus.js

--- a3/rhombus.js
+++ a3/rhombus.js
@@ -1,66 +1,194 @@
-function createRhombus() {
-  const height = parseInt(document.getElementById("rHeight").value);
-  const colorEven = document.getElementById("colorEven").value;
-  const colorOdd = document.getElementById("colorOdd").value;
-  const symbol = document.getElementById("symbol").value;
-
-  upRight(height, colorEven, colorOdd, symbol);
-  downRight(height, colorEven, colorOdd, symbol);
-  upLeft(height, colorEven, colorOdd, symbol);
-  downLeft(height, colorEven, colorOdd, symbol);
+// Individual functions for each section of the rhombus
+function generateUpRight(height, oddColor, evenColor, symbol, startPosition) {
+    let result = '';
+    let position = startPosition;
+    
+    for (let i = 1; i <= height; i++) {
+        let line = '';
+        let spaces = height - i;
+        let symbolsInRow = i;
+        
+        // Add leading spaces to center
+        for (let j = 0; j < spaces; j++) {
+            line += '&nbsp;';
+        }
+        
+        // Add symbols for right half of top section
+        for (let k = 0; k < symbolsInRow; k++) {
+            let color = (position % 2 === 1) ? oddColor : evenColor;
+            line += `<span style="color: ${color};">${symbol}</span>`;
+            position++;
+        }
+        
+        result += line + '<br>';
+    }
+    
+    return { html: result, endPosition: position };
 }
 
-function styledSymbol(pos, evenColor, oddColor, symbol) {
-  const color = (pos % 2 === 0) ? oddColor : evenColor;
-  return <span style="color:${color}">${symbol}</span>;
+function generateUpLeft(height, oddColor, evenColor, symbol, startPosition) {
+    let result = '';
+    let position = startPosition;
+    
+    for (let i = 1; i <= height; i++) {
+        let line = '';
+        let spaces = height - i;
+        let symbolsInRow = i - 1;
+        
+        // Add leading spaces
+        for (let j = 0; j < spaces; j++) {
+            line += '&nbsp;';
+        }
+        
+        // Add symbols for left half of top section
+        for (let k = 0; k < symbolsInRow; k++) {
+            let color = (position % 2 === 1) ? oddColor : evenColor;
+            line += `<span style="color: ${color};">${symbol}</span>`;
+            position++;
+        }
+        
+        result += line;
+    }
+    
+    return { html: result, endPosition: position };
 }
 
-function upRight(height, colorEven, colorOdd, symbol) {
-  let output = "";
-  for (let i = 0; i < height; i++) {
-    for (let j = 0; j <= i; j++) {
-      output += styledSymbol(j, colorEven, colorOdd, symbol);
+function generateDownLeft(height, oddColor, evenColor, symbol, startPosition) {
+    let result = '';
+    let position = startPosition;
+    
+    for (let i = height - 1; i >= 1; i--) {
+        let line = '';
+        let spaces = height - i;
+        let symbolsInRow = i - 1;
+        
+        // Add leading spaces
+        for (let j = 0; j < spaces; j++) {
+            line += '&nbsp;';
+        }
+        
+        // Add symbols for left half of bottom section
+        for (let k = 0; k < symbolsInRow; k++) {
+            let color = (position % 2 === 1) ? oddColor : evenColor;
+            line += `<span style="color: ${color};">${symbol}</span>`;
+            position++;
+        }
+        
+        result += line;
     }
-    output += "<br>";
-  }
-  document.getElementById("upRight").innerHTML = output;
+    
+    return { html: result, endPosition: position };
 }
 
-function downRight(height, colorEven, colorOdd, symbol) {
-  let output = "";
-  for (let i = height; i > 0; i--) {
-    for (let j = 0; j < i; j++) {
-      output += styledSymbol(j, colorEven, colorOdd, symbol);
+function generateDownRight(height, oddColor, evenColor, symbol, startPosition) {
+    let result = '';
+    let position = startPosition;
+    
+    for (let i = height - 1; i >= 1; i--) {
+        let line = '';
+        let spaces = height - i;
+        let symbolsInRow = i;
+        
+        // Add leading spaces
+        for (let j = 0; j < spaces; j++) {
+            line += '&nbsp;';
+        }
+        
+        // Add symbols for right half of bottom section
+        for (let k = 0; k < symbolsInRow; k++) {
+            let color = (position % 2 === 1) ? oddColor : evenColor;
+            line += `<span style="color: ${color};">${symbol}</span>`;
+            position++;
+        }
+        
+        result += line + '<br>';
     }
-    output += "<br>";
-  }
-  document.getElementById("downRight").innerHTML = output;
+    
+    return { html: result, endPosition: position };
 }
 
-function upLeft(height, colorEven, colorOdd, symbol) {
-  let output = "";
-  for (let i = 0; i < height; i++) {
-    for (let s = 0; s < height - i - 1; s++) {
-      output += "&nbsp;";
+function generateRhombusSections(height, oddColor, evenColor, symbol) {
+    let result = '';
+    let position = 1;
+    
+    // Generate each section and combine them
+    for (let i = 1; i <= height; i++) {
+        let line = '';
+        let spaces = height - i;
+        
+        // Add leading spaces
+        for (let j = 0; j < spaces; j++) {
+            line += '&nbsp;';
+        }
+        
+        // Left side (UpLeft section)
+        for (let k = 0; k < i - 1; k++) {
+            let color = (position % 2 === 1) ? oddColor : evenColor;
+            line += `<span style="color: ${color};">${symbol}</span>`;
+            position++;
+        }
+        
+        // Center symbol
+        let color = (position % 2 === 1) ? oddColor : evenColor;
+        line += `<span style="color: ${color};">${symbol}</span>`;
+        position++;
+        
+        // Right side (UpRight section)
+        for (let k = 0; k < i - 1; k++) {
+            let color = (position % 2 === 1) ? oddColor : evenColor;
+            line += `<span style="color: ${color};">${symbol}</span>`;
+            position++;
+        }
+        
+        result += line + '<br>';
     }
-    for (let j = 0; j <= i; j++) {
-      output += styledSymbol(j, colorEven, colorOdd, symbol);
+    
+    // Add tiny break between top and bottom halves
+    result += '<span style="font-size: 4px;"><br></span>';
+    
+    // Bottom half
+    for (let i = height - 1; i >= 1; i--) {
+        let line = '';
+        let spaces = height - i;
+        
+        // Add leading spaces
+        for (let j = 0; j < spaces; j++) {
+            line += '&nbsp;';
+        }
+        
+        // Left side (DownLeft section)
+        for (let k = 0; k < i - 1; k++) {
+            let color = (position % 2 === 1) ? oddColor : evenColor;
+            line += `<span style="color: ${color};">${symbol}</span>`;
+            position++;
+        }
+        
+        // Center symbol
+        let color = (position % 2 === 1) ? oddColor : evenColor;
+        line += `<span style="color: ${color};">${symbol}</span>`;
+        position++;
+        
+        // Right side (DownRight section)
+        for (let k = 0; k < i - 1; k++) {
+            let color = (position % 2 === 1) ? oddColor : evenColor;
+            line += `<span style="color: ${color};">${symbol}</span>`;
+            position++;
+        }
+        
+        result += line + '<br>';
     }
-    output += "<br>";
-  }
-  document.getElementById("upLeft").innerHTML = output;
+    
+    return result;
 }
 
-function downLeft(height, colorEven, colorOdd, symbol) {
-  let output = "";
-  for (let i = height; i > 0; i--) {
-    for (let s = 0; s < height - i; s++) {
-      output += "&nbsp;";
-    }
-    for (let j = 0; j < i; j++) {
-      output += styledSymbol(j, colorEven, colorOdd, symbol);
-    }
-    output += "<br>";
-  }
-  document.getElementById("downLeft").innerHTML = output;
-}
+document.getElementById('rhombusForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    const height = parseInt(document.getElementById('height').value);
+    const oddColor = document.getElementById('oddColor').value;
+    const evenColor = document.getElementById('evenColor').value;
+    const symbol = document.getElementById('symbol').value;
+    
+    const rhombusHTML = generateRhombusSections(height, oddColor, evenColor, symbol);
+    document.getElementById('rhombusDisplay').innerHTML = rhombusHTML;
+});
