25548ea84df75dd65951306288d5db220132c077
ved-p-coder <ved.patel@bellevuecollege.edu>
2025-05-21 18:53:18 -0700

    Update rhombus.js

--- a3/rhombus.js
+++ a3/rhombus.js
@@ -1,60 +1,25 @@
-// var rHeight =5;
-// var colorEven = "orange";
-// var colorOdd = "black";
-// var symbol ="*";
-
 function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
-upRight(pHeight, pColorEven, pColorOdd, pSymbol);
-downRight(pHeight, pColorEven, pColorOdd, pSymbol);
+  let rhombusStr = "";
 
+  // Top half
+  for (let i = 0; i < pHeight; i++) {
+    rhombusStr += "&nbsp;".repeat((pHeight - i - 1) * 2); // padding
+    for (let j = 0; j <= i; j++) {
+      const color = (j % 2 === 0) ? pColorOdd : pColorEven;
+      rhombusStr += `<span style="color:${color}">${pSymbol}</span>&nbsp;`;
+    }
+    rhombusStr += "<br>";
+  }
+
+  // Bottom half
+  for (let i = pHeight - 2; i >= 0; i--) {
+    rhombusStr += "&nbsp;".repeat((pHeight - i - 1) * 2);
+    for (let j = 0; j <= i; j++) {
+      const color = (j % 2 === 0) ? pColorOdd : pColorEven;
+      rhombusStr += `<span style="color:${color}">${pSymbol}</span>&nbsp;`;
+    }
+    rhombusStr += "<br>";
+  }
+
+  document.getElementById("rhombus").innerHTML = rhombusStr;
 }
-
-function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
-var rLine ="";
-for (i=0;i<pHeight;i++){
-rLine +="<p>";
-//Create each line on the Rhombus
-for(j=0;j<=i;j++){
-
-//Is the position even or odd so we change the color
-if (j%2)
-//even
-rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
-else
-//odd
-rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
-
-}
-rLine +="</p>";
-// console.log(rLine);
-
-}
-
-document.getElementById("upRight").innerHTML = rLine;
-}
-
-function downRight(pHeight, pColorEven, pColorOdd, pSymbol){
-var rLine ="";
-for (i=pHeight;i > 0;i--){
-rLine +="<p>";
-//Create each line on the Rhombus
-for(j=0;j<i;j++){
-
-//Is the position even or odd so we change the color
-if (j%2)
-//even
-rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
-else
-//odd
-rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
-
-}
-rLine +="</p>";
-// console.log(rLine);
-
-}
-
-document.getElementById("downRight").innerHTML = rLine;
-}
-
- 
