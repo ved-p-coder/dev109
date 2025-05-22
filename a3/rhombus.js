f9a0f908c533cc6242b163313fa5fc49fefedbe0
ved-p-coder <ved.patel@bellevuecollege.edu>
2025-05-21 19:34:43 -0700

    Update rhombus.js

--- a3/rhombus.js
+++ a3/rhombus.js
@@ -10,53 +10,57 @@ function createRhombus() {
   downLeft(height, colorEven, colorOdd, symbol);
 }
 
-function upRight(h, evenColor, oddColor, symbol) {
-  let result = "";
-  for (let i = 0; i < h; i++) {
-    for (let j = 0; j <= i; j++) {
-      result += styledSymbol(j, evenColor, oddColor, symbol);
-    }
-    result += "\n";
-  }
-  document.getElementById("upRight").innerHTML = result;
-}
-
-function downRight(h, evenColor, oddColor, symbol) {
-  let result = "";
-  for (let i = h; i > 0; i--) {
-    for (let j = 0; j < i; j++) {
-      result += styledSymbol(j, evenColor, oddColor, symbol);
-    }
-    result += "\n";
-  }
-  document.getElementById("downRight").innerHTML = result;
-}
-
-function upLeft(h, evenColor, oddColor, symbol) {
-  let result = "";
-  for (let i = 0; i < h; i++) {
-    for (let s = 0; s < h - i - 1; s++) result += " ";
-    for (let j = 0; j <= i; j++) {
-      result += styledSymbol(j, evenColor, oddColor, symbol);
-    }
-    result += "\n";
-  }
-  document.getElementById("upLeft").innerHTML = result;
-}
-
-function downLeft(h, evenColor, oddColor, symbol) {
-  let result = "";
-  for (let i = h; i > 0; i--) {
-    for (let s = 0; s < h - i; s++) result += " ";
-    for (let j = 0; j < i; j++) {
-      result += styledSymbol(j, evenColor, oddColor, symbol);
-    }
-    result += "\n";
-  }
-  document.getElementById("downLeft").innerHTML = result;
-}
-
 function styledSymbol(pos, evenColor, oddColor, symbol) {
   const color = (pos % 2 === 0) ? oddColor : evenColor;
   return `<span style="color:${color}">${symbol}</span>`;
+}
+
+function upRight(height, colorEven, colorOdd, symbol) {
+  let output = "";
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j <= i; j++) {
+      output += styledSymbol(j, colorEven, colorOdd, symbol);
+    }
+    output += "<br>";
+  }
+  document.getElementById("upRight").innerHTML = output;
+}
+
+function downRight(height, colorEven, colorOdd, symbol) {
+  let output = "";
+  for (let i = height; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      output += styledSymbol(j, colorEven, colorOdd, symbol);
+    }
+    output += "<br>";
+  }
+  document.getElementById("downRight").innerHTML = output;
+}
+
+function upLeft(height, colorEven, colorOdd, symbol) {
+  let output = "";
+  for (let i = 0; i < height; i++) {
+    for (let s = 0; s < height - i - 1; s++) {
+      output += "&nbsp;";
+    }
+    for (let j = 0; j <= i; j++) {
+      output += styledSymbol(j, colorEven, colorOdd, symbol);
+    }
+    output += "<br>";
+  }
+  document.getElementById("upLeft").innerHTML = output;
+}
+
+function downLeft(height, colorEven, colorOdd, symbol) {
+  let output = "";
+  for (let i = height; i > 0; i--) {
+    for (let s = 0; s < height - i; s++) {
+      output += "&nbsp;";
+    }
+    for (let j = 0; j < i; j++) {
+      output += styledSymbol(j, colorEven, colorOdd, symbol);
+    }
+    output += "<br>";
+  }
+  document.getElementById("downLeft").innerHTML = output;
 }
