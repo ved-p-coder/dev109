967a12bcf9a72c1d5841184969713ac67d1bcb2e
ved-p-coder <ved.patel@bellevuecollege.edu>
2025-05-22 17:13:07 -0700

    Update rhombus.js

--- a3/rhombus.js
+++ a3/rhombus.js
@@ -1,31 +1,40 @@
-function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
+function generateRhombus() {
+  const height = parseInt(document.getElementById('rHeight').value);
+  const colorEven = document.getElementById('colorEven').value;
+  const colorOdd = document.getElementById('colorOdd').value;
+  const symbol = document.getElementById('symbol').value;
+
+  const upperLeft = createHalf(height, colorEven, colorOdd, symbol, 'up', 'left');
+  const lowerLeft = createHalf(height, colorEven, colorOdd, symbol, 'down', 'left');
+  const upperRight = createHalf(height, colorEven, colorOdd, symbol, 'up', 'right');
+  const lowerRight = createHalf(height, colorEven, colorOdd, symbol, 'down', 'right');
+
   let output = "";
-  const totalHeight = pHeight * 2;
 
-  for (let i = 0; i < totalHeight; i++) {
-    let row = "";
+  for (let i = 0; i < height; i++) {
+    output += upperLeft[i] + "  " + upperRight[i] + "<br>";
+  }
 
-    const rowIndex = i < pHeight ? i : totalHeight - i - 1;
-    const numSymbols = rowIndex + 1;
-    const padding = pHeight - rowIndex - 1;
-
-    // Left side spacing
-    row += "&nbsp;".repeat(padding * 2);
-
-    // Left half
-    for (let j = 0; j < numSymbols; j++) {
-      const color = (j % 2 === 0) ? pColorOdd : pColorEven;
-      row += `<span style="color:${color};">${pSymbol}</span>`;
-    }
-
-    // Right half
-    for (let j = 0; j < numSymbols; j++) {
-      const color = (j % 2 === 0) ? pColorOdd : pColorEven;
-      row += `<span style="color:${color};">${pSymbol}</span>`;
-    }
-
-    output += row + "<br>";
+  for (let i = 0; i < height; i++) {
+    output += lowerLeft[i] + "  " + lowerRight[i] + "<br>";
   }
 
   document.getElementById("rhombus").innerHTML = output;
+}
+
+function createHalf(height, colorEven, colorOdd, symbol, vert, horiz) {
+  const lines = [];
+  for (let i = 0; i < height; i++) {
+    const lineIndex = vert === 'up' ? i : height - i - 1;
+    const spaces = " ".repeat(horiz === 'left' ? height - lineIndex - 1 : 0);
+    let line = spaces;
+
+    for (let j = 0; j <= lineIndex; j++) {
+      const color = j % 2 === 0 ? colorOdd : colorEven;
+      line += `<span style="color:${color}">${symbol}</span>`;
+    }
+
+    lines.push(line);
+  }
+  return lines;
 }
