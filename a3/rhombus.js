28cb21da67fb9fa36df6f4256ae93007d3cb6688
ved-p-coder <ved.patel@bellevuecollege.edu>
2025-05-22 17:03:49 -0700

    Update rhombus.js

--- a3/rhombus.js
+++ a3/rhombus.js
@@ -1,72 +1,58 @@
 function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
-  upLeft(pHeight, pColorEven, pColorOdd, pSymbol);
   upRight(pHeight, pColorEven, pColorOdd, pSymbol);
-  downLeft(pHeight, pColorEven, pColorOdd, pSymbol);
   downRight(pHeight, pColorEven, pColorOdd, pSymbol);
-}
-
-function upLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
-  let rLine = "";
-  for (let i = 0; i < pHeight; i++) {
-    rLine += "<p>";
-    for (let s = 0; s < pHeight - i - 1; s++) {
-      rLine += "<span style='color:white'>&nbsp;</span>";
-    }
-    for (let j = 0; j <= i; j++) {
-      if (j % 2)
-        rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
-      else
-        rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
-    }
-    rLine += "</p>";
-  }
-  document.getElementById("upLeft").innerHTML = rLine;
+  upLeft(pHeight, pColorEven, pColorOdd, pSymbol);
+  downLeft(pHeight, pColorEven, pColorOdd, pSymbol);
 }
 
 function upRight(pHeight, pColorEven, pColorOdd, pSymbol) {
   let rLine = "";
   for (let i = 0; i < pHeight; i++) {
-    rLine += "<p>";
+    rLine += "<p style='text-align: left;'>";
     for (let j = 0; j <= i; j++) {
-      if (j % 2)
-        rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
-      else
-        rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+      let color = (j % 2 === 0) ? pColorOdd : pColorEven;
+      rLine += `<span style='color:${color};'>${pSymbol}</span>`;
     }
     rLine += "</p>";
   }
   document.getElementById("upRight").innerHTML = rLine;
 }
 
-function downLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
-  let rLine = "";
-  for (let i = pHeight; i > 0; i--) {
-    rLine += "<p>";
-    for (let s = 0; s < pHeight - i; s++) {
-      rLine += "<span style='color:white'>&nbsp;</span>";
-    }
-    for (let j = 0; j < i; j++) {
-      if (j % 2)
-        rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
-      else
-        rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
-    }
-    rLine += "</p>";
-  }
-  document.getElementById("downLeft").innerHTML = rLine;
-}
-
 function downRight(pHeight, pColorEven, pColorOdd, pSymbol) {
   let rLine = "";
   for (let i = pHeight; i > 0; i--) {
-    rLine += "<p>";
+    rLine += "<p style='text-align: left;'>";
     for (let j = 0; j < i; j++) {
-      if (j % 2)
-        rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
-      else
-        rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+      let color = (j % 2 === 0) ? pColorOdd : pColorEven;
+      rLine += `<span style='color:${color};'>${pSymbol}</span>`;
     }
     rLine += "</p>";
   }
   document.getElementById("downRight").innerHTML = rLine;
+}
+
+function upLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
+  let rLine = "";
+  for (let i = 0; i < pHeight; i++) {
+    rLine += "<p style='text-align: right;'>";
+    for (let j = 0; j <= i; j++) {
+      let color = (j % 2 === 0) ? pColorOdd : pColorEven;
+      rLine += `<span style='color:${color};'>${pSymbol}</span>`;
+    }
+    rLine += "</p>";
+  }
+  document.getElementById("upLeft").innerHTML = rLine;
+}
+
+function downLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
+  let rLine = "";
+  for (let i = pHeight; i > 0; i--) {
+    rLine += "<p style='text-align: right;'>";
+    for (let j = 0; j < i; j++) {
+      let color = (j % 2 === 0) ? pColorOdd : pColorEven;
+      rLine += `<span style='color:${color};'>${pSymbol}</span>`;
+    }
+    rLine += "</p>";
+  }
+  document.getElementById("downLeft").innerHTML = rLine;
 }
